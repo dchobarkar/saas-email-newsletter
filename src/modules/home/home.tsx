a63bc27a992312d92512460add51701b7ff41415
Darshan Chobarkar <dchobarkar@gmail.com>
2024-06-26 12:25:02 +0530

    add featurehighlight component

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -4,6 +4,7 @@ import Header from "@/shared/widgets/header/header";
 import Banner from "@/modules/home/features/banner";
 import Branding from "@/modules/home/features/branding";
 import Benefits from "@/modules/home/features/benefits";
+import FeatureHighlight from "@/modules/home/features/featureHighlight";
 
 const Home = () => {
   return (
@@ -15,6 +16,8 @@ const Home = () => {
       <Branding />
 
       <Benefits />
+
+      <FeatureHighlight />
     </div>
   );
 };
