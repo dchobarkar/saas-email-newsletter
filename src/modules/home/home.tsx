2a7cdd56747b2658e3b3a386eeac04d20df6cf51
Darshan Chobarkar <dchobarkar@gmail.com>
2024-06-26 12:11:47 +0530

    add banner

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 import Header from "@/shared/widgets/header/header";
+import Banner from "@/modules/home/features/banner";
 
 const Home = () => {
   return (
     <div>
       <Header />
+
+      <Banner />
     </div>
   );
 };
