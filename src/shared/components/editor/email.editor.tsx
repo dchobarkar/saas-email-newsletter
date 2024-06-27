79e1555246f00368f5f67ef6fbea3803b9ae95a7
Darshan Chobarkar <dchobarkar@gmail.com>
2024-06-27 12:30:02 +0530

    add email design page

---
+++ src/shared/components/editor/email.editor.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+import toast from "react-hot-toast";
+import EmailEditor, { EditorRef, EmailEditorProps } from "react-email-editor";
+import { useRouter } from "next/navigation";
+import { Button } from "@nextui-org/react";
+import { useClerk } from "@clerk/nextjs";
+
+import { DefaultJsonData } from "@/assets/mails/default";
+
+const Emaileditor = ({ subjectTitle }: { subjectTitle: string }) => {
+  const [loading, setLoading] = useState(true);
+  const [jsonData, setJsonData] = useState<any | null>(DefaultJsonData);
+  const { user } = useClerk();
+  const history = useRouter();
+  const emailEditorRef = useRef<EditorRef>(null);
+
+  const exportHtml = () => {
+    const unlayer = emailEditorRef.current?.editor;
+
+    unlayer?.exportHtml(async (data) => {
+      const { design, html } = data;
+      setJsonData(design);
+    });
+  };
+
+  const onReady: EmailEditorProps["onReady"] = () => {
+    const unlayer: any = emailEditorRef.current?.editor;
+    unlayer.loadDesign(jsonData);
+  };
+
+  const saveDraft = async () => {
+    const unlayer = emailEditorRef.current?.editor;
+
+    unlayer?.exportHtml(async (data) => {
+      const { design } = data;
+    });
+  };
+
+  return (
+    <>
+      {!loading && (
+        <div className="w-full h-[90vh] relative">
+          <EmailEditor
+            minHeight={"80vh"}
+            ref={emailEditorRef}
+            onReady={onReady}
+          />
+
+          <div className="absolute bottom-0 flex items-center justify-end gap-4 right-0 w-full border-t p-3">
+            <Button
+              className="bg-transparent cursor-pointer flex items-center gap-1 text-black border border-[#00000048] text-lg rounded-lg"
+              onClick={saveDraft}
+            >
+              <span className="opacity-[.7]">Save Draft</span>
+            </Button>
+
+            <Button
+              className="bg-[#000] text-white cursor-pointer flex items-center gap-1 border text-lg rounded-lg"
+              onClick={exportHtml}
+            >
+              <span>Send</span>
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Emaileditor;
