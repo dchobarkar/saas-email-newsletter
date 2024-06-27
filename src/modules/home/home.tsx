f1688d0efd3b0ccaa42470a4e21c9ccf777fce3a
Darshan Chobarkar <dchobarkar@gmail.com>
2024-06-27 10:11:22 +0530

    fix feature to element

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 import Header from "@/shared/widgets/header/header";
-import Banner from "@/modules/home/features/banner";
-import Branding from "@/modules/home/features/branding";
-import Benefits from "@/modules/home/features/benefits";
-import FeatureHighlight from "@/modules/home/features/featureHighlight";
-import Pricing from "@/modules/home/features/pricing";
+import Banner from "@/modules/home/elements/banner";
+import Branding from "@/modules/home/elements/branding";
+import Benefits from "@/modules/home/elements/benefits";
+import FeatureHighlight from "@/modules/home/elements/featureHighlight";
+import Pricing from "@/modules/home/elements/pricing";
 import Footer from "@/shared/widgets/footer/footer";
 
 const Home = () => {
